814228aa38838ea2c1a5f0ca5357862597a5a2b1
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-03-06 12:46:14 +0100

    Add: Milestone 3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,40 +3,58 @@ import { useGlobalContext } from "../context/GlobalContext";
 const Card = () => {
 
     const { film, serie } = useGlobalContext()
+    const img = "https://image.tmdb.org/t/p/"
+
+    function Stars(vote) {
+        const roundedVote = Math.round(vote / 2)
+        const fullStar = "★";
+        const emptyStar = "☆";
+
+        return fullStar.repeat(roundedVote) + emptyStar.repeat(5 - roundedVote);
+    }
 
     return (
         <div>
-            <ul className="list-group">
+            <h2>Film</h2>
+            <div className="row row-cols-4 mx-auto">
                 {film.map((e) => {
                     return (
-                        <li className="list-group-item" key={e.id}>
+
+                        <div className="card" key={e.id}>
+                            <img className="card-img-top" src={img + "w342/" + e.poster_path} />
                             <>
                                 <h1>{e.title}</h1>
                                 <h2>{e.original_title}</h2>
-                                <h2>{e.original_language}</h2>
-                                <h2>{e.vote_average}</h2>
+                                <h2 className={`fi fi-${e.original_language = "en" ? "gb" : null}`}></h2>
+                                <h2>{Stars(e.vote_average)}</h2>
                             </>
-                        </li>
+                        </div>
                     )
                 }
                 )}
-            </ul>
+            </div>
 
-            <ul className="list-group">
-                {serie.map((e) => {
-                    return (
-                        <li className="list-group-item" key={e.id}>
-                            <>
-                                <h1>{e.name}</h1>
-                                <h2>{e.original_name}</h2>
-                                <h2>{e.original_language}</h2>
-                                <h2>{e.vote_average}</h2>
-                            </>
-                        </li>
-                    )
-                }
-                )}
-            </ul>
+
+            <div>
+                <h2>Serie TV</h2>
+                <div className="row row-cols-4 mx-auto">
+                    {serie.map((e) => {
+                        return (
+
+                            <div className="card" key={e.id}>
+                                <img className="card-img-top" src={img + "w342/" + e.poster_path} />
+                                <>
+                                    <h1>{e.name}</h1>
+                                    <h2>{e.name}</h2>
+                                    <h2 className={`fi fi-${e.original_language = "en" ? "gb" : null}`}></h2>
+                                    <h2>{Stars(e.vote_average)}</h2>
+                                </>
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
